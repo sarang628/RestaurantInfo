8651d4cfb00a37b7224276159e8442aaf3e3d9c1
sryang <sarang628@gmail.com>
2025-06-07 13:24:16 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.sarang.torang
 
-import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import com.sarang.torang.di.image.customImageLoader
-import com.sarang.torang.di.restaurant_info.RestaurantInfoWithPermissionWithLocation
+import com.sarang.torang.di.image1.customImageLoader
 import com.sarang.torang.di.restaurant_info.restaurantInfo
 import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
---
+++ app/src/main/java/com/sarang/torang/di/image1/ProvideTorangAsyncImage.kt
@@ -0,0 +1,95 @@
+package com.sarang.torang.di.image1
+
+import TorangAsyncImage
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.sarang.torang.ImageLoader1
+import com.sarang.torang.di.pinchzoom.ImageLoader
+import com.sarang.torang.di.pinchzoom.PinchZoomState
+import com.sarang.torang.di.pinchzoom.ZoomSnapshot
+import com.sarang.torang.di.pinchzoom.isZooming
+import com.sarang.torang.di.pinchzoom.pinchZoomAndTransform
+import kotlinx.coroutines.flow.distinctUntilChanged
+
+
+typealias TorangAsyncImageType = @Composable (
+    modifier: Modifier,
+    text: String,
+    width: Dp?,
+    height: Dp?,
+    contentScale: ContentScale?
+) -> Unit
+
+typealias ZoomableTorangAsyncImage = @Composable (
+    modifier: Modifier,
+    text: String,
+    width: Dp?,
+    height: Dp?,
+    contentScale: ContentScale?,
+    originHeight: Dp?
+) -> Unit
+
+fun provideTorangAsyncImage(): TorangAsyncImageType =
+    { modifier, model, progressSize, errorIconSize, contentScale ->
+        TorangAsyncImage(
+            modifier = modifier,
+            model = model,
+            progressSize = progressSize ?: 50.dp,
+            errorIconSize = errorIconSize ?: 50.dp,
+            contentScale = contentScale ?: ContentScale.Fit
+        )
+    }
+
+fun provideZoomableTorangAsyncImage(onZoomState: (PinchZoomState) -> Unit = {}): ZoomableTorangAsyncImage =
+    { modifier, model, progressSize, errorIconSize, contentScale, originHeight ->
+        val zoomState =
+            remember {
+                PinchZoomState(
+                    originHeight = originHeight?.value ?: 0f,
+                    url = model
+                )
+            }
+
+        LaunchedEffect(zoomState) {
+            snapshotFlow {
+                ZoomSnapshot(
+                    zoomState.accumulateZoom.value,
+                    zoomState.offset.value,
+                    zoomState.isZooming
+                )
+            }.distinctUntilChanged()
+                .collect {
+                    onZoomState(zoomState)
+                }
+        }
+
+        TorangAsyncImage(
+            modifier = modifier.pinchZoomAndTransform(zoomState),
+            model = model,
+            progressSize = progressSize ?: 50.dp,
+            errorIconSize = errorIconSize ?: 50.dp,
+            contentScale = contentScale ?: ContentScale.Crop
+        )
+    }
+
+
+/**
+ * 핀치줌 라이브러리 제공용 이미지로더
+ */
+fun provideImageLoader(): ImageLoader =
+    { modifier: Modifier,
+      url: String,
+      contentScale: ContentScale? ->
+        provideTorangAsyncImage().invoke(modifier, url, 30.dp, 30.dp, contentScale)
+    }
+
+val customImageLoader: ImageLoader1 = { modifier, url, width, height, scale ->
+    // 여기서 실제 이미지 로딩 구현 예시
+    provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
+}
