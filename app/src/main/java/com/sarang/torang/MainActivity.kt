4db21c50a2276f898fcc113d30dd9910a68f56dc
sryang <sarang628@gmail.com>
2025-09-08 14:04:33 +0800

    add permission process

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sarang.torang
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import com.sarang.torang.di.image3.customImageLoader
 import com.sarang.torang.di.restauarnt_info_di.restaurantInfo
 import com.sryang.torang.ui.TorangTheme
@@ -20,8 +23,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             TorangTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    CompositionLocalProvider(LocalRestaurantInfoImageLoader provides customImageLoader, LocalRestaurantInfo provides restaurantInfo())
-                    {
+                    if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                    }
+                    CompositionLocalProvider(LocalRestaurantInfoImageLoader provides customImageLoader,
+                        LocalRestaurantInfo provides restaurantInfo()) {
                         //RestaurantInfo_(restaurantId = 234)
                         //RestaurantInfoWithPermissionWithLocation(234)
                         LocalRestaurantInfo.current.invoke(234, {}, {}, {})
