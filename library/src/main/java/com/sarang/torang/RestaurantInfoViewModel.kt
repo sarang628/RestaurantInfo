49cc805277a0748b1b2a6f565db34974c888be21
sryang <sarang628@gmail.com>
2025-09-08 17:53:08 +0800

    add refresh

--- library/src/main/java/com/sarang/torang/RestaurantInfoViewModel.kt
+++ library/src/main/java/com/sarang/torang/RestaurantInfoViewModel.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,11 +20,20 @@ class RestaurantInfoViewModel @Inject constructor(
     var uiState: RestaurantInfoUiState by mutableStateOf(RestaurantInfoUiState.Loading); private set
     var myLatitude : Double = 0.0
     var myLongitude : Double = 0.0
+    var isRefresh : Boolean by mutableStateOf(false); private set
 
     suspend fun fetchRestaurantInfo1(restaurantId: Int) {
         try { uiState = RestaurantInfoUiState.Success(getRestaurantInfoUseCase.invoke(restaurantId)
             .copy(myLatitude = myLatitude, myLongitude = myLongitude))}
         catch (e : Exception){ Log.e(tag, "$e") }
+    }
+
+    fun refresh(restaurantId : Int){
+        isRefresh = true
+        viewModelScope.launch {
+            fetchRestaurantInfo1(restaurantId)
+            isRefresh = false
+        }
     }
 
     fun setCurrentLocation(latitude: Double, longitude: Double) {
